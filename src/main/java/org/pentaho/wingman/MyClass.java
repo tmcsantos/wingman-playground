ff25d4fbb51678344daf613e51675d7b8e02052f
Tiago Santos <tsantos@pentaho.com>
2017-08-25 14:08:19 +0100

    Initial commit

---
+++ src/main/java/org/pentaho/wingman/MyClass.java
@@ -0,0 +1,14 @@
+package org.pentaho.wingman;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyClass {
+  private static final Logger logger = LoggerFactory.getLogger(MyClass.class);
+
+  public String sayHello() {
+    logger.debug("saying 'Hello' ...");
+    return "Hello";
+  }
+
+}
